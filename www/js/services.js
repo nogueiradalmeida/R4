b6b54d9f6461b56943568a22e9034d52e07db3d5
Jose Nogueira <nogueiradalmeida@gmail.com>
2017-02-03 00:32:08 -0200

    <commit message>

---
+++ www/js/services.js
@@ -0,0 +1,51 @@
+var app = angular.module('starter.services', []);
+
+app.factory('Chats', function($http) {
+  // Might use a resource here that returns a JSON array
+
+
+  // Some fake testing data
+  var chats = [{
+    id: 0,
+    name: 'IBOV',
+    date: '12/2010',
+    value: '7.8'
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
+    face: 'img/max.png'
+  }, {
+    id: 2,
+    name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
+    face: 'img/adam.jpg'
+  }, {
+    id: 3,
+    name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
+    face: 'img/perry.png'
+  }, {
+    id: 4,
+    name: 'Mike Harrington',
+    lastText: 'This is wicked good ice cream.',
+    face: 'img/mike.png'
+  }];
+
+  return {
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
+        }
+      }
+      return null;
+    }
+  };
+});
